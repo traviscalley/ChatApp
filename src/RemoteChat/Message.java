203d9fc936bce2c8e2d170d2794f29098d33c677
Parker Berberian <pdb1005@wildcats.unh.edu>
2019-05-06 12:09:06 -0400

    socket server impl

--- src/RemoteChat/Message.java
+++ src/RemoteChat/Message.java
@@ -6,22 +6,23 @@ import java.util.List;
 
 public class Message {
 
-    private int likes, dislikes;
+    private int likes;
     private boolean deleted;
     private final long  id;
     private final long parent;
+    private final User poster;
     private String content;
     private final ArrayList<Long> children;
 
-    public Message(long parent, String message, long id){
+    public Message(long parent, String message, long id, User user){
         synchronized (this) {
             likes = 0;
-            dislikes = 0;
             this.parent = parent;
             this.content = message;
             this.id = id;
             this.children = new ArrayList<>();
             deleted = false;
+            poster = user;
         }
     }
 
@@ -30,15 +31,11 @@ public class Message {
     }
 
     public synchronized int dislike(){
-        return ++dislikes;
+        return --likes;
     }
 
     public synchronized int getLikes() {
         return likes;
-    }
-
-    public synchronized int getDislikes() {
-        return dislikes;
     }
 
     public synchronized boolean isDeleted() {
@@ -61,9 +58,13 @@ public class Message {
         return children;
     }
 
+    public synchronized void addChild(long id) {
+        children.add(id);
+    }
+
     @Override
     public String toString(){
-        return getContent();
+        return poster.name + ": " + "[" + getLikes() + "] - " + getContent() + " - [" + getId() + "]";
     }
 
     //TODO: define equality and hash
